21f631b7530b45e202460345480a5bf74ed1b4f9
Andres Suarez <zertosh@gmail.com>
2017-02-11 17:02:37 -0500

    Fix module initialization

--- file-system-blob-store.js
+++ file-system-blob-store.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const fs = require('fs-plus')
+const fs = require('fs')
 const path = require('path')
 
 module.exports =
--- v8-compile-cache.js
+++ v8-compile-cache.js
@@ -1,6 +1,22 @@
-blobStore = FileSystemBlobStore.load(
-  path.join(process.env.ATOM_HOME, 'blob-store/')
-)
+'use strict';
+
+const path = require('path');
+const os = require('os');
+
+const mkdirp = require('mkdirp');
+
+const FileSystemBlobStore = require('./file-system-blob-store');
+const NativeCompileCache = require('./native-compile-cache');
+
+const cacheDir = path.join(os.tmpdir(), 'v8-compile-cache');
+mkdirp.sync(cacheDir);
+
+const blobStore = FileSystemBlobStore.load(cacheDir);
+
 NativeCompileCache.setCacheStore(blobStore)
 NativeCompileCache.setV8Version(process.versions.v8)
 NativeCompileCache.install()
+
+process.on('exit', (code) => {
+  blobStore.save();
+});
