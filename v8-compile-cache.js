ebcfdbe9706b9fa0968ab6bf663abaeaf6f232e1
Andres Suarez <zertosh@gmail.com>
2017-02-17 19:33:24 -0500

    Fix module entry to use instances

--- v8-compile-cache.js
+++ v8-compile-cache.js
@@ -7,12 +7,12 @@ const FileSystemBlobStore = require('./FileSystemBlobStore');
 const NativeCompileCache = require('./NativeCompileCache');
 
 const cacheDir = path.join(os.tmpdir(), 'v8-compile-cache');
-const blobStore = FileSystemBlobStore.load(cacheDir);
+const blobStore = new FileSystemBlobStore(cacheDir);
 
-NativeCompileCache.setCacheStore(blobStore)
-NativeCompileCache.setV8Version(process.versions.v8)
-NativeCompileCache.install()
+NativeCompileCache.setCacheStore(blobStore);
+NativeCompileCache.setV8Version(process.versions.v8);
+NativeCompileCache.install();
 
-process.on('exit', (code) => {
+process.on('exit', code => {
   blobStore.save();
 });
