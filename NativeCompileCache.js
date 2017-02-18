fcbc30c3053a93b46a9732b95db912f2e3578128
Andres Suarez <zertosh@gmail.com>
2017-02-18 16:24:54 -0500

    Pull out module compile from NativeCompileCache

--- NativeCompileCache.js
+++ NativeCompileCache.js
@@ -2,25 +2,16 @@
 
 const Module = require('module');
 const path = require('path');
-const vm = require('vm');
-const crypto = require('crypto');
 
-function computeHash(contents) {
-  return crypto.createHash('sha1').update(contents, 'utf8').digest('hex');
-}
+const moduleCompile = require('./moduleCompile');
 
 class NativeCompileCache {
   constructor() {
-    this.cacheStore = null;
     this.previousModuleCompile = null;
   }
 
-  setCacheStore(store) {
-    this.cacheStore = store;
-  }
-
-  setV8Version(v8Version) {
-    this.v8Version = v8Version.toString();
+  setCacheStore(cacheStore) {
+    this.cacheStore = cacheStore;
   }
 
   install() {
@@ -37,20 +28,14 @@ class NativeCompileCache {
   }
 
   overrideModuleCompile() {
-    const self = this;
-    let resolvedArgv = null;
-    // Here we override Node's module.js
-    // (https://github.com/atom/node/blob/atom/lib/module.js#L378), changing
-    // only the bits that affect compilation in order to use the cached one.
+    const cacheStore = this.cacheStore;
     Module.prototype._compile = function(content, filename) {
-      const moduleSelf = this;
-      // remove shebang
-      content = content.replace(/^#!.*/, '');
+      const self = this;
       function require(path) {
-        return moduleSelf.require(path);
+        return self.require(path);
       }
       require.resolve = function(request) {
-        return Module._resolveFilename(request, moduleSelf);
+        return Module._resolveFilename(request, self);
       };
       require.main = process.mainModule;
 
@@ -60,78 +45,13 @@ class NativeCompileCache {
 
       const dirname = path.dirname(filename);
 
-      // create wrapper function
-      const wrapper = Module.wrap(content);
+      const compiledWrapper = moduleCompile(cacheStore, filename, content);
 
-      const cacheKey = filename;
-      const invalidationKey = computeHash(wrapper + self.v8Version);
-      let compiledWrapper = null;
-      if (self.cacheStore.has(cacheKey, invalidationKey)) {
-        const buffer = self.cacheStore.get(cacheKey, invalidationKey);
+      // We skip the debugger setup because by the time we run, node has already
+      // done that itself.
 
-        const script = new vm.Script(wrapper, {
-          filename,
-          lineOffset: 0,
-          displayErrors: true,
-          cachedData: buffer,
-          produceCachedData: true,
-        });
-
-        if (script.cachedDataRejected) {
-          self.cacheStore.delete(cacheKey);
-          // TODO: replace code
-        }
-
-        compiledWrapper = script.runInThisContext({
-          filename,
-          lineOffset: 0,
-          columnOffset: 0,
-          displayErrors: true,
-        });
-      } else {
-        let script;
-        try {
-          script = new vm.Script(wrapper, {
-            filename,
-            lineOffset: 0,
-            displayErrors: true,
-            produceCachedData: true,
-          });
-          compiledWrapper = script.runInThisContext({
-            filename,
-            lineOffset: 0,
-            columnOffset: 0,
-            displayErrors: true,
-          });
-        } catch (err) {
-          console.error(`Error running script ${filename}`);
-          throw err;
-        }
-        if (script.cachedDataProduced) {
-          self.cacheStore.set(cacheKey, invalidationKey, script.cachedData);
-        }
-      }
-      if (global.v8debug) {
-        if (!resolvedArgv) {
-          // we enter the repl if we're not given a filename argument.
-          if (process.argv[1]) {
-            resolvedArgv = Module._resolveFilename(process.argv[1], null);
-          } else {
-            resolvedArgv = 'repl';
-          }
-        }
-
-        // Set breakpoint on module start
-        if (filename === resolvedArgv) {
-          // Installing this dummy debug event listener tells V8 to start
-          // the debugger.  Without it, the setBreakPoint() fails with an
-          // 'illegal access' error.
-          global.v8debug.Debug.setListener(() => {});
-          global.v8debug.Debug.setBreakPoint(compiledWrapper, 0, 0);
-        }
-      }
-      const args = [moduleSelf.exports, require, moduleSelf, filename, dirname, process, global];
-      return compiledWrapper.apply(moduleSelf.exports, args);
+      const args = [self.exports, require, self, filename, dirname, process, global];
+      return compiledWrapper.apply(self.exports, args);
     };
   }
 
--- v8-compile-cache.js
+++ v8-compile-cache.js
@@ -6,11 +6,10 @@ const os = require('os');
 const FileSystemBlobStore = require('./FileSystemBlobStore');
 const NativeCompileCache = require('./NativeCompileCache');
 
-const cacheDir = path.join(os.tmpdir(), 'v8-compile-cache');
+const cacheDir = path.join(os.tmpdir(), 'v8-compile-cache', process.versions.v8);
 const blobStore = new FileSystemBlobStore(cacheDir);
 
 NativeCompileCache.setCacheStore(blobStore);
-NativeCompileCache.setV8Version(process.versions.v8);
 NativeCompileCache.install();
 
 process.on('exit', code => {
