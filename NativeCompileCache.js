2f05244971fd92c3f934addf6692644b01ae30e8
Andres Suarez <zertosh@gmail.com>
2017-02-20 23:10:07 -0500

    Merge moduleCompile back into NativeCompileCache

--- NativeCompileCache.js
+++ NativeCompileCache.js
@@ -1,29 +1,30 @@
 'use strict';
 
 const Module = require('module');
+const crypto = require('crypto');
 const path = require('path');
-
-const moduleCompile = require('./moduleCompile');
+const vm = require('vm');
 
 module.exports = class NativeCompileCache {
   constructor() {
+    this._cacheStore = null;
     this._previousModuleCompile = null;
   }
 
   setCacheStore(cacheStore) {
-    this.cacheStore = cacheStore;
+    this._cacheStore = cacheStore;
   }
 
   install() {
-    const nativeCompileCache = this;
+    const self = this;
     this._previousModuleCompile = Module.prototype._compile;
     Module.prototype._compile = function(content, filename) {
-      const self = this;
+      const mod = this;
       function require(id) {
-        return self.require(id);
+        return mod.require(id);
       }
       require.resolve = function(request) {
-        return Module._resolveFilename(request, self);
+        return Module._resolveFilename(request, mod);
       };
       require.main = process.mainModule;
 
@@ -33,18 +34,81 @@ module.exports = class NativeCompileCache {
 
       const dirname = path.dirname(filename);
 
-      const compiledWrapper =
-        moduleCompile(nativeCompileCache.cacheStore, filename, content);
+      const compiledWrapper = self._moduleCompile(filename, content);
 
       // We skip the debugger setup because by the time we run, node has already
       // done that itself.
 
-      const args = [self.exports, require, self, filename, dirname, process, global];
-      return compiledWrapper.apply(self.exports, args);
+      const args = [mod.exports, require, mod, filename, dirname, process, global];
+      return compiledWrapper.apply(mod.exports, args);
     };
   }
 
   uninstall() {
     Module.prototype._compile = this._previousModuleCompile;
+  }
+
+  _moduleCompile(filename, content) {
+    // https://github.com/nodejs/node/blob/v7.5.0/lib/module.js#L511
+
+    // Remove shebang
+    var contLen = content.length;
+    if (contLen >= 2) {
+      if (content.charCodeAt(0) === 35/*#*/ &&
+          content.charCodeAt(1) === 33/*!*/) {
+        if (contLen === 2) {
+          // Exact match
+          content = '';
+        } else {
+          // Find end of shebang line and slice it off
+          var i = 2;
+          for (; i < contLen; ++i) {
+            var code = content.charCodeAt(i);
+            if (code === 10/*\n*/ || code === 13/*\r*/) break;
+          }
+          if (i === contLen) {
+            content = '';
+          } else {
+            // Note that this actually includes the newline character(s) in the
+            // new output. This duplicates the behavior of the regular
+            // expression that was previously used to replace the shebang line
+            content = content.slice(i);
+          }
+        }
+      }
+    }
+
+    // create wrapper function
+    var wrapper = Module.wrap(content);
+
+    var invalidationKey = crypto
+      .createHash('sha1')
+      .update(content, 'utf8')
+      .digest('hex');
+
+    var buffer = this._cacheStore.get(filename, invalidationKey);
+
+    var script = new vm.Script(wrapper, {
+      filename: filename,
+      lineOffset: 0,
+      displayErrors: true,
+      cachedData: buffer,
+      produceCachedData: true,
+    });
+
+    if (script.cachedDataProduced) {
+      this._cacheStore.set(filename, invalidationKey, script.cachedData);
+    } else if (script.cachedDataRejected) {
+      this._cacheStore.delete(filename);
+    }
+
+    const compiledWrapper = script.runInThisContext({
+      filename: filename,
+      lineOffset: 0,
+      columnOffset: 0,
+      displayErrors: true,
+    });
+
+    return compiledWrapper;
   }
 };
