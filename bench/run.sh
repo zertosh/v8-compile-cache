0acfd7f8a0c506e2ceb5ce2992f29e0a1f138e24
Andres Suarez <zertosh@gmail.com>
2017-04-27 22:09:31 -0400

    Make benchmark node binary configurable

--- bench/run.sh
+++ bench/run.sh
@@ -5,8 +5,11 @@
 # rm -rf "$TMPDIR/v8-compile-cache"
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+: ${NODE_BIN:=node}
+
+$NODE_BIN -p '`node ${process.versions.node}, v8 ${process.versions.v8}`'
 
 for f in $THIS_DIR/require-*.js; do
   echo "Running "$(basename $f)""
-  for i in {1..5}; do node $f; done
+  for i in {1..5}; do $NODE_BIN $f; done
 done
